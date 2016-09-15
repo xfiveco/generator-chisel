36adaf72da8d69ff81d027782d215285b0b2675c
Michal Grabowski <michalgrabowski@Internship.local>
2016-09-15 13:44:37 +0200

    Watchify errors handling

--- generators/app/templates/gulp/helpers.js
+++ generators/app/templates/gulp/helpers.js
@@ -1,10 +1,12 @@
 'use strict';
 
+// Globally omit stack trace
+Error.stackTraceLimit = 0;
 var helpers = function (gulp, plugins, config) {
   return {
     onError: function (error) {
       console.error(error.message + '\n');
-      console.error(error.fileName + ':' + error.lineNumber + '\n');
+      console.error(error.stack + '\n');
 
       this.emit('end');
     }
