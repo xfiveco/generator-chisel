5646a24c995fbb9f21e77c0d5317f9ee5dcc099e
Jakub Bogucki <jakub300@gmail.com>
2016-10-03 13:24:19 +0200

    Improve Browsersync config generation

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')({ pattern: '*' });
 var config  = require('./package.json').chisel;
+var generator_config = require('./.yo-rc.json')['generator-chisel'].config;
 var helpers = require('./gulp/helpers')(gulp, plugins, config);
 <% if (features.has_wp) { %>
 config = require('./gulp/wpConfig')(config);
@@ -12,7 +13,7 @@ config = require('./gulp/wpConfig')(config);
  */
 plugins.glob.sync('gulp/tasks/*').forEach(function (path) {
   path = path.replace('gulp/', './gulp/');
-  require(path)(gulp, plugins, config, helpers);
+  require(path)(gulp, plugins, config, helpers, generator_config);
 });
 
 gulp.task('default', ['serve', 'watchify']);
