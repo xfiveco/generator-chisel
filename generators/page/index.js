7113ca339d175b7f9a786ee7e39fc7be98bd74a2
Jakub Bogucki <jakub300@gmail.com>
2016-11-28 18:17:02 +0100

    Simplify WP Page creation

--- generators/page/index.js
+++ generators/page/index.js
@@ -145,8 +145,7 @@ var PageChisel = yeoman.Base.extend({
         slug = json.post_name;
         cb(!slug);
       },
-      (cb) => wpCli(['post', 'update', String(id), {post_status: 'publish'}], cb),
-      (stdio, cb) => {
+      (cb) => {
         var pack = JSON.parse(this.fs.read(this.destinationPath('package.json')));
         var chisel = pack.chisel, dest = chisel.dest;
         var templates = path.join(dest.wordpress, 'wp-content/themes',
