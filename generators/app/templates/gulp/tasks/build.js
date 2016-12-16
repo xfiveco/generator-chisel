d8b8aa50231c8aa903c4060890522dacbbe05889
Jakub Bogucki <jakub300@gmail.com>
2016-12-16 18:24:41 +0100

    Remove template related things on WordPress projects

--- generators/app/templates/gulp/tasks/build.js
+++ generators/app/templates/gulp/tasks/build.js
@@ -6,7 +6,11 @@ var buildTask = function (gulp, plugins, config) {
     return plugins.del([config.dest.base]);
   });
 
+  <% if(projectType == 'wp-with-fe') { %>
+  gulp.task('build', plugins.sequence('styles-build', 'lint-js', 'scripts-build'))
+  <% } else { %>
   gulp.task('build', plugins.sequence('styles-build', 'lint-js', 'scripts-build', 'templates-build', 'validate-html'))
+  <% } %>
 };
 
 module.exports = buildTask;
--- generators/app/templates/gulp/tasks/serve.js
+++ generators/app/templates/gulp/tasks/serve.js
@@ -3,7 +3,12 @@
 var path = require('path');
 
 var serveTask = function (gulp, plugins, config, helpers, generator_config) {
-  gulp.task('serve', ['styles-watch', 'templates-watch', 'assets-watch'], function() {
+  <% if(projectType == 'wp-with-fe') { %>
+  var startTasks = ['styles-watch', 'assets-watch'];
+  <% } else { %>
+  var startTasks = ['styles-watch', 'templates-watch', 'assets-watch'];
+  <% } %>
+  gulp.task('serve', startTasks, function() {
     <% if(projectType == 'wp-with-fe') { %>
     var name = generator_config.nameSlug;
     var browserSyncConfig = {
@@ -24,7 +29,9 @@ var serveTask = function (gulp, plugins, config, helpers, generator_config) {
     plugins.browserSync.init(browserSyncConfig);
 
     gulp.watch(path.join(config.src.base, config.src.styles), ['styles-watch']);
+    <% if(projectType == 'fe') { %>
     gulp.watch(config.src.templatesWatch, ['templates-watch']);
+    <% } %>
     gulp.watch(path.join(config.src.base, config.src.assets), ['assets-watch']);
     <% if(projectType == 'wp-with-fe') { %>
     gulp.watch('**/*.{php,twig}').on('change', plugins.browserSync.reload);
