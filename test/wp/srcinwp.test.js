fb6950b229044d54afe6a7c97d62269e86d1b1b5
Jakub Bogucki <jakub300@gmail.com>
2017-03-28 22:14:03 +0200

    Add tests for src inside WordPress theme

---
+++ test/wp/srcinwp.test.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var helpers = require('yeoman-test');
+var assert = require('yeoman-assert');
+
+describe('Chisel Generator with WordPress (subgenerator, src inside theme)', function () {
+  before(function (done) {
+    this.timeout(240000)
+
+    // We skip those tests when running locally because they
+    // require database at 127.0.0.1 with root user and no password.
+    if(!process.env.TRAVIS) {
+      this.skip(); return;
+    }
+
+    helpers
+      .run(path.join(__dirname, '../../generators/app'))
+      .withOptions({
+        'skip-install': true,
+        'run-wp': true
+      })
+      .withPrompts({
+        name: 'Test Project SRC in WP',
+        author: 'Test Author',
+        projectType: 'wp-with-fe',
+        features: [],
+        databasePassword: new String(''),
+        adminPassword: 'pass',
+        adminEmail: 'user@example.com',
+        plugins: [],
+        srcPlacement: 'theme'
+      })
+      .on('end', done);
+  });
+
+  it('should have no src in main directory', function(done) {
+    assert.noFile('src');
+
+    done();
+  })
+
+  it('should move base src into theme directory', function(done) {
+    var base = 'wp/wp-content/themes/test-project-src-in-wp/src';
+    assert.file([
+      base,
+      base+'/assets/images/.keep',
+      base+'/scripts/app.js',
+      base+'/styles/main.scss',
+      base+'/styles/settings/_global.scss',
+      base+'/styles/components/_header.scss'
+    ]);
+
+    done();
+  })
+
+  it('should move WP-specific styles into theme directory', function(done) {
+    var base = 'wp/wp-content/themes/test-project-src-in-wp/src';
+    assert.file([
+      base+'/styles/components/_comment.scss',
+      base+'/styles/components/_post.scss'
+    ]);
+
+    done();
+  })
+});
