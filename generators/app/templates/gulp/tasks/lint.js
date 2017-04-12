85087e6cd778ba23197c2c3f9504a724257be26c
Artur Kot <arturkot85@gmail.com>
2017-04-12 12:24:39 +0200

    Add vendor dirs, exclude them from linting

--- generators/app/templates/gulp/tasks/lint.js
+++ generators/app/templates/gulp/tasks/lint.js
@@ -4,14 +4,14 @@ var path = require('path');
 
 var lintTask = function (gulp, plugins, config) {
   gulp.task('lint-js', function() {
-    return gulp.src([path.join(config.src.base, config.src.scripts), '!node_modules/**'])
+    return gulp.src([path.join(config.src.base, config.src.scripts), '!node_modules/**', '!src/scripts/vendor/**'])
       .pipe(plugins.eslint())
       .pipe(plugins.eslint.format())
       .pipe(plugins.eslint.failAfterError());
   });
 
   gulp.task('lint-css', function() {
-    return gulp.src(path.join(config.src.base, config.src.styles))
+    return gulp.src([path.join(config.src.base, config.src.styles), '!src/styles/vendor/**'])
       .pipe(plugins.stylelint({
         reporters: [
           {formatter: 'string', console: true}
