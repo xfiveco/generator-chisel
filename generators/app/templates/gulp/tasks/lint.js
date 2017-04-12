2df3590b406b0544bc146bcb6235c89dc86f2e81
Artur Kot <arturkot85@gmail.com>
2017-04-12 15:34:49 +0200

    Styelint, Eslint: use ignore files instead of gulp tasks

--- generators/app/templates/gulp/tasks/lint.js
+++ generators/app/templates/gulp/tasks/lint.js
@@ -4,14 +4,14 @@ var path = require('path');
 
 var lintTask = function (gulp, plugins, config) {
   gulp.task('lint-js', function() {
-    return gulp.src([path.join(config.src.base, config.src.scripts), '!node_modules/**', '!src/scripts/vendor/**'])
+    return gulp.src(path.join(config.src.base, config.src.scripts))
       .pipe(plugins.eslint())
       .pipe(plugins.eslint.format())
       .pipe(plugins.eslint.failAfterError());
   });
 
   gulp.task('lint-css', function() {
-    return gulp.src([path.join(config.src.base, config.src.styles), '!src/styles/vendor/**'])
+    return gulp.src(path.join(config.src.base, config.src.styles))
       .pipe(plugins.stylelint({
         reporters: [
           {formatter: 'string', console: true}
