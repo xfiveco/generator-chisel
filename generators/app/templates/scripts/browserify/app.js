1368b2695c3c4b3fdc8e312fc1c96e1542e69d7b
Lubos Kmetko <lubos@xfive.co>
2017-05-16 17:13:30 +0200

    Rewriting sample module to ES6

--- generators/app/templates/scripts/browserify/app.js
+++ generators/app/templates/scripts/browserify/app.js
@@ -2,7 +2,9 @@
   Project: <%= name %>
   Author: <%= author %>
  */
-
+<% if (features.has_babel) { %>
+import { greeting as greet } from './greeting';<% } else { %>
 var greet = require('./greeting.js')
+<% } %>
 
 greet('World');
--- generators/app/templates/scripts/browserify/greeting.js
+++ generators/app/templates/scripts/browserify/greeting.js
@@ -1,17 +1,29 @@
-'use strict';<% if (features.has_jquery) { %>
+<% if (features.has_babel) { %>
+export const greeting = name => {
+  const element = document.querySelector('.js-greeting');
 
-var $ = require('jquery');<% } %>
-
-var greeting = function (name) {<% if (features.has_jquery) { %>
-  var element = $('.js-greeting');<% } else { %>
-  var element = document.querySelector('.js-greeting');<% } %>
-<% if (features.has_jquery) { %>
-  if (element.length) {
-    element.text(name);
-  }<% } else { %>
   if (element) {
     element.innerHTML = name;
-  }<% } %>
+  }
 };
+<% } else { %>
+'use strict';
+<% if (features.has_jquery) { %>
+var $ = require('jquery');
 
-module.exports = greeting;
+var greeting = function (name) {
+  var element = $('.js-greeting');
+
+  if (element.length) {
+    element.text(name);
+  }
+};<% } else { %>
+var greeting = function (name) {
+  var element = document.querySelector('.js-greeting');
+
+  if (element) {
+    element.innerHTML = name;
+  }
+};
+<% } %>
+module.exports = greeting;<% } %>
