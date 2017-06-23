efa3707427a630acd25b42357ee40c94c617043e
Jakub Bogucki <jakub300@gmail.com>
2017-06-23 13:08:16 +0200

    Add support for local .yo-rc.json

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,11 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
+var _ = require('lodash');
 var plugins = require('gulp-load-plugins')({ pattern: '*' });
 var config  = require('./package.json').chisel;
 var generator_config = require('./.yo-rc.json')['generator-chisel'].config;
 var helpers = require('./gulp/helpers')(gulp, plugins, config);
 config = require('./gulp/prepareConfig')(config);
+
+try {
+  var generator_config_local = require('./.yo-rc-local.json')['generator-chisel'].config;
+  _.merge(generator_config, generator_config_local);
+} catch(e) {}
+
 /**
  * Batch tasks loader
  */
