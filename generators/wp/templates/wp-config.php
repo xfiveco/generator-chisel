23f7a15d794d0028894adeefb0aae37b6904a600
Lubos Kmetko <lubos@xfive.co>
2017-07-18 10:20:28 +0200

    Whitespace fixes

--- generators/wp/templates/wp-config.php
+++ generators/wp/templates/wp-config.php
@@ -25,13 +25,15 @@ if ( file_exists( dirname( __FILE__ ) . '/wp-config-local.php' ) ) {
 	 */
 
 	include dirname( __FILE__ ) . '/wp-config-local.php';
+
 } else {
 
 	/**
 	 * Settings for non-local environments, used when wp-config-local.php not available
 	 */
 
-// ** MySQL settings - You can get this info from your web host ** //
+	/** MySQL settings - You can get this info from your web host */
+
 	/** The name of the database for WordPress */
 	define( 'DB_NAME', $_SERVER['DB_NAME'] );
 
@@ -43,7 +45,6 @@ if ( file_exists( dirname( __FILE__ ) . '/wp-config-local.php' ) ) {
 
 	/** MySQL hostname */
 	define( 'DB_HOST', $_SERVER['DB_HOST'] );
-
 
 	/**
 	 * WordPress Database Table prefix.
@@ -77,7 +78,7 @@ if ( file_exists( dirname( __FILE__ ) . '/wp-config-local.php' ) ) {
  */
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -108,10 +109,10 @@ if ( !WP_DEBUG ) {
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+	define( 'ABSPATH', dirname(__FILE__) . '/' );
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once( ABSPATH . 'wp-settings.php' );
 
 /** Disable the Plugin and Theme Editor */
-define('DISALLOW_FILE_EDIT', true);
+define( 'DISALLOW_FILE_EDIT', true );
