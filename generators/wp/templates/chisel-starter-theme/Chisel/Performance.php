2dc11caafe03d94a1e5620939dfe28a084635e8d
Lubos Kmetko <lubos@xfive.co>
2017-08-31 11:06:33 +0200

    Allowing to define which scripts should be deferred or asynced

--- generators/wp/templates/chisel-starter-theme/Chisel/Performance.php
+++ generators/wp/templates/chisel-starter-theme/Chisel/Performance.php
@@ -9,8 +9,32 @@ namespace Chisel;
  * Default performance settings for Chisel
  */
 class Performance {
+
+	private $scriptsToAsync = array( 'wp-embed.min.js' );
+	// private $scriptsToDefer = array( 'script1.js', 'script2.js' );
+
 	public function __construct() {
-		add_filter('script_loader_tag', array($this, 'asyncScripts'));
+		add_filter( 'script_loader_tag', array( $this, 'asyncScripts' ) );
+		// add_filter( 'script_loader_tag', array( $this, 'deferScripts' ) );
+	}
+
+	/**
+	 * Async scripts
+	 * @param  string $tag Script tag
+	 * @return string      Script tag
+	 */
+	public function asyncScripts( $tag ) {
+		if ( is_admin() ) {
+			return $tag;
+		}
+
+		foreach ( $this->scriptsToAsync as $script ) {
+			if ( strpos( $tag, $script ) == true ) {
+				return str_replace( ' src', ' async src', $tag );
+			}
+		}
+
+		return $tag;
 	}
 
 	/**
@@ -18,11 +42,18 @@ class Performance {
 	 * @param  string $tag Script tag
 	 * @return string      Script tag
 	 */
-	public function asyncScripts($tag) {
-		if (is_admin()) {
+	public function deferScripts( $tag ) {
+		if ( is_admin() ) {
 			return $tag;
 		}
-		return str_replace( ' src', ' async src', $tag );
+
+		foreach ( $this->scriptsToDefer as $script ) {
+			if ( strpos( $tag, $script ) == true ) {
+				return str_replace( ' src', ' defer src', $tag );
+			}
+		}
+
+		return $tag;
 	}
 
 }
