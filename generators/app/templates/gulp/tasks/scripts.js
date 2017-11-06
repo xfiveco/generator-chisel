903ab429b3cd50077316c7143a747a088ffb9bdf
Jakub Bogucki <jakub300@gmail.com>
2017-11-06 18:51:29 +0100

    Reload in callback

--- generators/app/templates/gulp/tasks/scripts.js
+++ generators/app/templates/gulp/tasks/scripts.js
@@ -3,26 +3,30 @@
 const path = require('path');
 const _ = require('lodash');
 const webpack = require('webpack');
-
-const RELOAD_DELAY_MS = 100;
+const browserSync = require('browser-sync');
 
 let webpackConfig;
 
-function webpackDone(err, stats) {
+function webpackDone(isDev) {
   // Based on https://github.com/webpack/webpack-cli/blob/1eb340f4f32bb5303de9355d51e0bcf712755c0b/bin/webpack.js
-  if (err) {
-    console.error(err.stack || err);
-    if (err.details) {
-      console.error(err.details);
+  return (err, stats) => {
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+      process.exit(1); // eslint-disable-line
     }
-    process.exit(1); // eslint-disable-line
+    let statsConfig = webpackConfig.stats || 'normal';
+    if (typeof statsConfig == 'string') {
+      statsConfig = stats.constructor.presetToOptions(statsConfig);
+      Object.assign(statsConfig, { colors: true });
+    }
+    console.log(stats.toString(statsConfig));
+    if(isDev) {
+      browserSync.reload();
+    }
   }
-  let statsConfig = webpackConfig.stats || 'normal';
-  if (typeof statsConfig == 'string') {
-    statsConfig = stats.constructor.presetToOptions(statsConfig);
-    Object.assign(statsConfig, { colors: true });
-  }
-  console.log(stats.toString(statsConfig));
 }
 
 var scriptsTask = function(gulp, plugins, config, helpers) {
@@ -33,24 +37,18 @@ var scriptsTask = function(gulp, plugins, config, helpers) {
   });
 
   gulp.task('scripts-watch', ['scripts-load-config'], function() {
-    const scriptsDest = path.join(config.dest.base, config.dest.scripts);
-
     gulp
       .src(path.join(config.src.base, config.src.scriptsMain))
       .pipe(plugins.vinylNamed())
-      .pipe(plugins.webpackStream(webpackConfig, webpack, webpackDone))
-      .pipe(gulp.dest(scriptsDest))
-
-    gulp
-      .watch(path.join(scriptsDest, '/**'))
-      .on('change', _.debounce(plugins.browserSync.reload, RELOAD_DELAY_MS));
+      .pipe(plugins.webpackStream(webpackConfig, webpack, webpackDone(true)))
+      .pipe(gulp.dest(path.join(config.dest.base, config.dest.scripts)))
   });
 
   gulp.task('scripts-build', ['scripts-load-config'], function() {
     return gulp
       .src(path.join(config.src.base, config.src.scriptsMain))
       .pipe(plugins.vinylNamed())
-      .pipe(plugins.webpackStream(webpackConfig, webpack, webpackDone))
+      .pipe(plugins.webpackStream(webpackConfig, webpack, webpackDone(false)))
       .pipe(helpers.stealWebpackManifest())
       .pipe(plugins.sourcemaps.init({ loadMaps: true }))
       .pipe(
