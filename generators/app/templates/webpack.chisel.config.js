ffb1d2804f4f5111f942ba495e60f6b8815bc5d1
Jakub Bogucki <jakub300@gmail.com>
2017-11-15 17:29:07 +0100

    wrap generation in function, add defineplugin, improve stats, disable node

--- generators/app/templates/webpack.chisel.config.js
+++ generators/app/templates/webpack.chisel.config.js
@@ -8,46 +8,56 @@ const path = require('path');
 const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
 const NameAllModulesPlugin = require('./gulp/NameAllModulesPlugin');
 
-const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
-const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+function createConfig(cb) {
+  const isDevelopment = process.env.NODE_ENV === 'development';
+  const isProduction = process.env.NODE_ENV === 'production';
 
-const config = {
-  output: {
-    filename: '[id].bundle.js',
-    chunkFilename: '[id].chunk.js',
-  },
-  externals: {<% if (has_jquery_vendor_config) { %>
-    jquery: 'window.jQuery',
-  <% } %>},
-  devtool: 'inline-source-map',
-  stats: 'normal',
-  watch: IS_DEVELOPMENT,
-  module: {<% if (features.has_babel) { %>
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
-  <% } %>},
-  plugins: [
-    new webpack.HashedModuleIdsPlugin(),
-    new webpack.NamedModulesPlugin(),
-    new webpack.NamedChunksPlugin(chunk => {
-      if (chunk.name) {
-        return chunk.name;
-      }
-      return `${chunk
-        .mapModules(m => {
-          const file = path.relative(m.context, m.request);
-          return `${path.basename(file, path.extname(file))}-${m.id}`;
-        })
-        .join('_')}.c`;
-    }),
-    new NameAllModulesPlugin(),
-    new ChunkManifestPlugin({
-      filename: `manifest${!IS_PRODUCTION ? '-dev' : ''}.json`,
-    }),
-    new webpack.optimize.ModuleConcatenationPlugin(),
-  ],
-};
+  const config = {
+    output: {
+      filename: '[id].bundle.js',
+      chunkFilename: '[id].chunk.js',
+    },
+    externals: {<% if (has_jquery_vendor_config) { %>
+      jquery: 'window.jQuery',
+    <% } %>},
+    devtool: 'inline-source-map',
+    stats: { colors: true, modules: false },
+    watch: isDevelopment,
+    module: {<% if (features.has_babel) { %>
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      ],
+    <% } %>},
+    node: false,
+    plugins: [
+      new webpack.HashedModuleIdsPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new webpack.NamedChunksPlugin(chunk => {
+        if (chunk.name) {
+          return chunk.name;
+        }
+        return `${chunk
+          .mapModules(m => {
+            const file = path.relative(m.context, m.request);
+            return `${path.basename(file, path.extname(file))}-${m.id}`;
+          })
+          .join('_')}.c`;
+      }),
+      new NameAllModulesPlugin(),
+      new ChunkManifestPlugin({
+        filename: `manifest${!isProduction ? '-dev' : ''}.json`,
+      }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      }),
+    ],
+  };
+
+  cb(config);
+}
 
 module.exports = () =>
   new Promise(resolve => {
-    resolve(config);
+    createConfig(resolve);
   });
