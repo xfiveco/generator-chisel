cfcf156fffdcb538a31cce21170ff3035db83e3e
Jakub Bogucki <jakub300@gmail.com>
2017-11-20 14:54:10 +0100

    Format serve.js

--- generators/app/templates/gulp/tasks/serve.js
+++ generators/app/templates/gulp/tasks/serve.js
@@ -1,55 +1,53 @@
 'use strict';
 
-var path = require('path');
+const path = require('path');
 
-var serveTask = function (gulp, plugins, config, helpers, generator_config) {
-  <% if(projectType == 'wp-with-fe') { %>
-  var startTasks = ['styles-watch', 'assets-watch', 'vendor-watch'];
-  <% } else { %>
-  var startTasks = ['styles-watch', 'assets-watch', 'vendor-watch'];
-  gulp.task('vendor-rebuild-template', ['vendor-watch'], function () {
+// eslint-disable-next-line no-unused-vars
+function serveTask(gulp, plugins, config, helpers, generatorConfig) {<% if(projectType == 'wp-with-fe') { %>
+  const startTasks = ['styles-watch', 'assets-watch', 'vendor-watch'];<% } else { %>
+  const startTasks = ['styles-watch', 'assets-watch', 'vendor-watch'];
+
+  gulp.task('vendor-rebuild-template', ['vendor-watch'], () => {
     gulp.start('templates-watch');
-  });
-  <% } %>
-  gulp.task('serve', startTasks, function() {
-    <% if(projectType == 'wp-with-fe') { %>
-    var name = generator_config.nameSlug;
-    var browserSyncConfig = {
+  });<% } %>
+
+  gulp.task('serve', startTasks, () => {<% if(projectType == 'wp-with-fe') { %>
+    const name = generatorConfig.nameSlug;
+    const browserSyncConfig = {
       proxy: {
-        target: generator_config.proxyTarget || name+'.test',
+        target: generatorConfig.proxyTarget || `${name}.test`,
         reqHeaders: {
-          'x-chisel-proxy': '1'
-        }
+          'x-chisel-proxy': '1',
+        },
       },
       ghostMode: false,
-      online: true
-    }
-    <% } else { %>
+      online: true,
+    };<% } else { %>
     gulp.start('templates-watch');
 
-    var browserSyncConfig = {
+    const browserSyncConfig = {
       server: './',
       ghostMode: false,
-      online: true
-    }
-    <% } %>
+      online: true,
+    };<% } %>
+
     plugins.browserSync.init(browserSyncConfig);
 
-    gulp.watch(path.join(config.src.base, config.src.styles), ['styles-watch']);
-    <% if(projectType == 'fe') { %>
+    gulp.watch(path.join(config.src.base, config.src.styles), ['styles-watch']);<% if(projectType == 'fe') { %>
     gulp.watch(config.src.templatesWatch, ['templates-watch']);
-    gulp.watch(path.join(config.src.base, config.src.vendorConfig), ['vendor-rebuild-template']);
-    <% } %>
-    gulp.watch(path.join(config.src.base, config.src.assets), ['assets-watch']);
-    <% if(projectType == 'wp-with-fe') { %>
-    gulp.watch(config.src.templatesWatch).on('change', plugins.browserSync.reload);
-    gulp.watch(path.join(config.src.base, config.src.vendorConfig), function () {
-      gulp.start('vendor-watch', function () {
+    gulp.watch(path.join(config.src.base, config.src.vendorConfig), [
+      'vendor-rebuild-template'
+    ]);<% } %>
+    gulp.watch(path.join(config.src.base, config.src.assets), ['assets-watch']);<% if(projectType == 'wp-with-fe') { %>
+    gulp
+      .watch(config.src.templatesWatch)
+      .on('change', plugins.browserSync.reload);
+    gulp.watch(path.join(config.src.base, config.src.vendorConfig), () => {
+      gulp.start('vendor-watch', () => {
         plugins.browserSync.reload();
       });
-    });
-    <% } %>
+    });<% } %>
   });
-};
+}
 
 module.exports = serveTask;
