0fb8193bd788981a19c25c74cf9369e02d95ba91
Jakub Bogucki <jakub300@gmail.com>
2017-11-20 14:17:24 +0100

    Make gulpfile and gulp/helpers pass eslint and prettier

--- generators/app/templates/gulp/helpers.js
+++ generators/app/templates/gulp/helpers.js
@@ -1,27 +1,29 @@
 'use strict';
 
-var through = require('through2');
-var path = require('path');
-var webpackManifestFile;
+const through = require('through2');
+const path = require('path');
 
-var helpers = function (gulp, plugins, config) {
+let webpackManifestFile;
+
+function helpers() {
   return {
-    onError: function (error) {
-      console.error(error.message + '\n');
-      console.error(error.stack + '\n');
+    onError(error) {
+      console.error(error.message);
+      console.error(error.stack);
 
       this.emit('end');
     },
-    removeSourceMap: function() {
-      return through.obj(function(file, enc, callback) {
+    removeSourceMap() {
+      return through.obj(function removeSourceMap(file, enc, callback) {
+        // eslint-disable-next-line no-param-reassign
         delete file.sourceMap;
         this.push(file);
         callback();
       });
     },
-    stealWebpackManifest: function() {
-      return through.obj(function(file, enc, callback) {
-        if(file.basename == 'manifest.json') {
+    stealWebpackManifest() {
+      return through.obj(function stealWebpackManifest(file, enc, callback) {
+        if (file.basename === 'manifest.json') {
           webpackManifestFile = file;
         } else {
           this.push(file);
@@ -29,34 +31,39 @@ var helpers = function (gulp, plugins, config) {
         callback();
       });
     },
-    prepareWebpackManifest: function() {
-      var pathMap = {};
-      return through.obj(function(file, enc, callback) {
-        this.push(file);
-        if(file.path && file.revOrigPath) {
-          pathMap[path.basename(file.revOrigPath)] = path.basename(file.path);
-        }
-        callback();
-      }, function(callback) {
-        if(!webpackManifestFile) {
-          callback();
-          return;
-        }
-        var webpackManifest = JSON.parse(webpackManifestFile.contents);
-        Object.keys(webpackManifest).forEach((chunkName) => {
-          const fileName = webpackManifest[chunkName];
-          const newName = pathMap[fileName];
-          if(!newName) {
-            throw new Error('could not find generated file '+fileName);
+    prepareWebpackManifest() {
+      const pathMap = {};
+      return through.obj(
+        function createPathMap(file, enc, callback) {
+          this.push(file);
+          if (file.path && file.revOrigPath) {
+            pathMap[path.basename(file.revOrigPath)] = path.basename(file.path);
           }
-          webpackManifest[chunkName] = newName;
-        });
-        webpackManifestFile.contents = Buffer.from(JSON.stringify(webpackManifest));
-        this.push(webpackManifestFile);
-        callback();
-      });
-    }
-  }
-};
+          callback();
+        },
+        function prepareWebpackManifest(callback) {
+          if (!webpackManifestFile) {
+            callback();
+            return;
+          }
+          const webpackManifest = JSON.parse(webpackManifestFile.contents);
+          Object.keys(webpackManifest).forEach(chunkName => {
+            const fileName = webpackManifest[chunkName];
+            const newName = pathMap[fileName];
+            if (!newName) {
+              throw new Error(`could not find generated file ${fileName}`);
+            }
+            webpackManifest[chunkName] = newName;
+          });
+          webpackManifestFile.contents = Buffer.from(
+            JSON.stringify(webpackManifest)
+          );
+          this.push(webpackManifestFile);
+          callback();
+        }
+      );
+    },
+  };
+}
 
 module.exports = helpers;
