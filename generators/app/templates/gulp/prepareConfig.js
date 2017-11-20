f070a5ddea5ba12b7d17af1c46658e6507843529
Artur Kot <arturkot85@gmail.com>
2017-11-20 12:51:03 +0100

    Make prepareConfig pass Eslint

--- generators/app/templates/gulp/prepareConfig.js
+++ generators/app/templates/gulp/prepareConfig.js
@@ -1,33 +1,37 @@
-
-var path = require('path');
-
-/**
-* WordPress destination paths should be inside WP template
-*/
+const path = require('path');
+<% if (projectType == 'wp-with-fe') { %>
+// WordPress destination paths should be inside WP template
 function prepareWpConfig(config) {
-  var dest = config.dest;
-  dest.base = path.join(dest.wordpress, 'wp-content/themes',
-    dest.wordpressTheme, dest.base);
-  
-  var wordpressTemplatePath = path.join(config.dest.wordpress, 'wp-content/themes', config.dest.wordpressTheme);
-  config.src.templatesWatch = [
-    path.join(wordpressTemplatePath, config.src.templatesPath, '**/*.twig'),
-    path.join(wordpressTemplatePath, '**/*.php')
+  const { dest, src } = config;
+  const wordpressTemplatePath = path.join(
+    dest.wordpress,
+    'wp-content/themes',
+    dest.wordpressTheme
+  );
+
+  dest.base = path.join(
+    dest.wordpress,
+    'wp-content/themes',
+    dest.wordpressTheme,
+    dest.base
+  );
+
+  src.templatesWatch = [
+    path.join(wordpressTemplatePath, src.templatesPath, '**/*.twig'),
+    path.join(wordpressTemplatePath, '**/*.php'),
   ];
-  
+
   return config;
 }
-
+<% } %>
 function prepareConfig(config) {
+  const { <% if (projectType != 'wp-with-fe') { %>templatesWatch, <% } %>templatesBuild, base } = config.src;
+<% if (projectType == 'wp-with-fe') { %>
+  Object.assign(config, prepareWpConfig(config));<% } else { %>
+  templatesWatch[0] = path.join(base, templatesWatch[0]);
+  templatesWatch[1] = path.join(base, templatesWatch[1]);<% } %>
+  templatesBuild[0] = path.join(base, templatesBuild[0]);
 
-  <% if (projectType == 'wp-with-fe') { %>
-  config = prepareWpConfig(config);
-  <% } else { %>
-  config.src.templatesWatch[0] = path.join(config.src.base, config.src.templatesWatch[0]);
-  config.src.templatesWatch[1] = path.join(config.src.base, config.src.templatesWatch[1]);
-  <% } %>
-  
-  config.src.templatesBuild[0] = path.join(config.src.base, config.src.templatesBuild[0]);
   return config;
 }
 
