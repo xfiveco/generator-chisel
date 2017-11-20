6c881a5c39342b2d43e262f0f3f412af4e9c5206
Artur Kot <arturkot85@gmail.com>
2017-11-20 13:50:34 +0100

    Make tasks/assets.js pass Eslint

--- generators/app/templates/gulp/tasks/assets.js
+++ generators/app/templates/gulp/tasks/assets.js
@@ -1,26 +1,30 @@
 'use strict';
 
-var path = require('path');
+const path = require('path');
 
-var assetsTask = function (gulp, plugins, config, helpers) {
-  gulp.task('assets-clean', function() {
-    return plugins.del([path.join(config.dest.base, config.dest.assets)]);
-  })
+module.exports = function assetsTask(gulp, plugins, config) {
+  const { dest, src } = config.dest;
 
-  gulp.task('assets-build', ['assets-clean'], function () {
-    return gulp.src([path.join(config.src.base, config.src.assets), '!**/.keep'],
-      { base: config.src.base })
-      .pipe(gulp.dest(config.dest.base))
-      .on('end', plugins.browserSync.reload);
-  });
+  gulp.task('assets-clean', () =>
+    plugins.del([path.join(dest.base, dest.assets)])
+  );
 
-  gulp.task('assets-watch', function () {
-    return gulp.src([path.join(config.src.base, config.src.assets), '!**/.keep'],
-      { base: config.src.base })
-      .pipe(plugins.newer(config.dest.base))
-      .pipe(gulp.dest(config.dest.base))
-      .on('end', plugins.browserSync.reload);
-  });
+  gulp.task('assets-build', ['assets-clean'], () =>
+    gulp
+      .src([path.join(src.base, src.assets), '!**/.keep'], {
+        base: src.base,
+      })
+      .pipe(gulp.dest(dest.base))
+      .on('end', plugins.browserSync.reload)
+  );
+
+  gulp.task('assets-watch', () =>
+    gulp
+      .src([path.join(src.base, src.assets), '!**/.keep'], {
+        base: src.base,
+      })
+      .pipe(plugins.newer(dest.base))
+      .pipe(gulp.dest(dest.base))
+      .on('end', plugins.browserSync.reload)
+  );
 };
-
-module.exports = assetsTask;
