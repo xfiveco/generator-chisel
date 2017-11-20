2ed55675ac1bfd2eb8063162e9e7cdb5e8e50a09
Jakub Bogucki <jakub300@gmail.com>
2017-11-20 15:08:05 +0100

    Format templates

--- generators/app/templates/gulp/tasks/templates.js
+++ generators/app/templates/gulp/tasks/templates.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var templatesFunctions = require('../templatesFunctions');
+const fs = require('fs');
+const path = require('path');
+const templatesFunctions = require('../templatesFunctions');
 
-var templatesTask = function (gulp, plugins, config, helpers) {
-
+function templatesTask(gulp, plugins, config, helpers) {
   function getTemplateInputData(file) {
+    let data;
     try {
-      var data = JSON.parse(
+      data = JSON.parse(
         fs.readFileSync(
-          path.join(config.src.base, config.src.dataPath,
-            path.basename(file.path) + '.json')
+          path.join(
+            config.src.base,
+            config.src.dataPath,
+            `${path.basename(file.path)}.json`
+          )
         )
       );
     } catch (error) {
@@ -24,35 +27,40 @@ var templatesTask = function (gulp, plugins, config, helpers) {
   }
 
   function templates(manifest) {
-    var buildIncludedGlobs = config.src.templatesBuild || '';
-    var buildIncludedFilter = plugins.filter(buildIncludedGlobs);
+    const buildIncludedGlobs = config.src.templatesBuild || '';
+    const buildIncludedFilter = plugins.filter(buildIncludedGlobs);
 
-    return gulp.src(config.src.templatesWatch)
+    return gulp
+      .src(config.src.templatesWatch)
       .on('end', plugins.browserSync.reload)
       .pipe(plugins.plumber(helpers.onError))
       .pipe(buildIncludedFilter)
       .pipe(plugins.data(getTemplateInputData))
-      .pipe(plugins.twig({
-        base: path.join(config.src.base, config.src.templatesPath),
-        functions: templatesFunctions({
-          config: config,
-          manifest: manifest
-        }),
-        errorLogToConsole: true
-      }))
+      .pipe(
+        plugins.twig({
+          base: path.join(config.src.base, config.src.templatesPath),
+          functions: templatesFunctions({
+            config,
+            manifest,
+          }),
+          errorLogToConsole: true,
+        })
+      )
       .pipe(plugins.prettify({ indent_size: 2, preserve_newlines: true }))
       .pipe(gulp.dest(config.dest.base));
   }
 
-  gulp.task('templates-watch', function() {
-    return templates();
-  });
+  gulp.task('templates-watch', () => templates());
 
-  gulp.task('templates-build', function() {
-    var manifest = JSON.parse(fs.readFileSync(
-      path.join(config.dest.base, config.dest.revManifest), 'utf8'));
+  gulp.task('templates-build', () => {
+    var manifest = JSON.parse(
+      fs.readFileSync(
+        path.join(config.dest.base, config.dest.revManifest),
+        'utf8'
+      )
+    );
     return templates(manifest);
   });
-};
+}
 
 module.exports = templatesTask;
