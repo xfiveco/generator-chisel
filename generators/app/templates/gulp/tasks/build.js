5f38e5f860fb3446735cdd40be9f539153fe2777
Artur Kot <arturkot85@gmail.com>
2017-11-20 14:11:55 +0100

    Make tasks/build.js pass Eslint

--- generators/app/templates/gulp/tasks/build.js
+++ generators/app/templates/gulp/tasks/build.js
@@ -1,16 +1,28 @@
 'use strict';
 
-var buildTask = function (gulp, plugins, config) {
-
-  gulp.task('clean', function() {
-    return plugins.del([config.dest.base]);
-  });
-
-  <% if(projectType == 'wp-with-fe') { %>
-  gulp.task('build', plugins.sequence('clean', 'styles-build', 'lint-js', 'scripts-build', 'vendor-build'))
-  <% } else { %>
-  gulp.task('build', plugins.sequence('clean', 'styles-build', 'lint-js', 'scripts-build', 'vendor-build', 'templates-build', 'validate-html'))
-  <% } %>
+module.exports = function buildTask(gulp, plugins, config) {
+  gulp.task('clean', () => plugins.del([config.dest.base]));
+<% if(projectType == 'wp-with-fe') { %>
+  gulp.task(
+    'build',
+    plugins.sequence(
+      'clean',
+      'styles-build',
+      'lint-js',
+      'scripts-build',
+      'vendor-build'
+    )
+  );<% } else { %>
+  gulp.task(
+    'build',
+    plugins.sequence(
+      'clean',
+      'styles-build',
+      'lint-js',
+      'scripts-build',
+      'vendor-build',
+      'templates-build',
+      'validate-html'
+    )
+  );<% } %>
 };
-
-module.exports = buildTask;
