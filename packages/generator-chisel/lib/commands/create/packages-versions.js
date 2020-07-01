b24068f7bb730762450512fc1535c4cc6315fff9
Jakub Bogucki <jakub.bogucki@xfive.co>
2020-07-01 22:47:11 +0200

    Publish
     - babel-preset-chisel@1.0.0-alpha.0
     - chisel-plugin-code-style@1.0.0-alpha.0
     - chisel-plugin-wordpress@1.0.0-alpha.0
     - chisel-scripts@1.0.0-alpha.0
     - chisel-shared-utils@1.0.0-alpha.0
     - eslint-config-chisel@3.0.0-alpha.0
     - generator-chisel@1.0.0-alpha.0
     - stylelint-config-chisel@1.0.0-alpha.0

--- packages/generator-chisel/lib/commands/create/packages-versions.js
+++ packages/generator-chisel/lib/commands/create/packages-versions.js
@@ -1,10 +1,10 @@
 module.exports = {
-  'babel-preset-chisel': '0.0.0',
-  'chisel-plugin-code-style': '0.0.0',
-  'chisel-plugin-wordpress': '0.0.0',
-  'chisel-scripts': '0.0.0',
-  'chisel-shared-utils': '0.0.0',
-  'eslint-config-chisel': '2.0.0',
-  'generator-chisel': '0.0.0',
-  'stylelint-config-chisel': '0.0.0',
+  'babel-preset-chisel': '1.0.0-alpha.0',
+  'chisel-plugin-code-style': '1.0.0-alpha.0',
+  'chisel-plugin-wordpress': '1.0.0-alpha.0',
+  'chisel-scripts': '1.0.0-alpha.0',
+  'chisel-shared-utils': '1.0.0-alpha.0',
+  'eslint-config-chisel': '3.0.0-alpha.0',
+  'generator-chisel': '1.0.0-alpha.0',
+  'stylelint-config-chisel': '1.0.0-alpha.0',
 };
