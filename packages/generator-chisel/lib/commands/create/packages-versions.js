800027d5045b6386cb58412009a8a1b7993fae33
Jakub Bogucki <jakub.bogucki@xfive.co>
2020-07-03 17:01:52 +0200

    Publish
     - chisel-plugin-wordpress@1.0.0-alpha.2
     - generator-chisel@1.0.0-alpha.2

--- packages/generator-chisel/lib/commands/create/packages-versions.js
+++ packages/generator-chisel/lib/commands/create/packages-versions.js
@@ -1,10 +1,10 @@
 module.exports = {
   'babel-preset-chisel': '1.0.0-alpha.0',
   'chisel-plugin-code-style': '1.0.0-alpha.0',
-  'chisel-plugin-wordpress': '1.0.0-alpha.1',
+  'chisel-plugin-wordpress': '1.0.0-alpha.2',
   'chisel-scripts': '1.0.0-alpha.0',
   'chisel-shared-utils': '1.0.0-alpha.0',
   'eslint-config-chisel': '3.0.0-alpha.0',
-  'generator-chisel': '1.0.0-alpha.1',
+  'generator-chisel': '1.0.0-alpha.2',
   'stylelint-config-chisel': '1.0.0-alpha.0',
 };
