26722ac053e8daf5271d3920409528165bbe182e
Jakub Bogucki <jakub.bogucki@xfive.co>
2020-07-03 17:28:36 +0200

    Publish
     - generator-chisel@1.0.0-alpha.3

--- packages/generator-chisel/lib/commands/create/packages-versions.js
+++ packages/generator-chisel/lib/commands/create/packages-versions.js
@@ -5,6 +5,6 @@ module.exports = {
   'chisel-scripts': '1.0.0-alpha.0',
   'chisel-shared-utils': '1.0.0-alpha.0',
   'eslint-config-chisel': '3.0.0-alpha.0',
-  'generator-chisel': '1.0.0-alpha.2',
+  'generator-chisel': '1.0.0-alpha.3',
   'stylelint-config-chisel': '1.0.0-alpha.0',
 };
