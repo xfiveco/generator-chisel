a4cff3169154d5c442974efb80291860641cfec5
Jakub Bogucki <jakub.bogucki@xfive.co>
2020-08-03 16:03:25 +0200

    Improve config template

--- packages/generator-chisel/lib/commands/create/creators/app/template/chisel.config.chisel-tpl.js
+++ packages/generator-chisel/lib/commands/create/creators/app/template/chisel.config.chisel-tpl.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 const creatorData = <%= JSON.stringify(creatorData) %>;
 
 <%
@@ -31,10 +33,10 @@ module.exports = {
   },
   `  %>
 
-  // To use React and hot reload for react components:
+  // To use React and hot reload for React components:
   // 1. Run `yarn add react-hot-loader @hot-loader/react-dom`
   // 3. Mark your root component as hot-exported as described on
-  //    https://github.com/gaearon/react-hot-loader
+  //    https://github.com/gaearon/react-hot-loader#getting-started (step 2)
   // 4. Uncomment line below
   // react: true,
 
@@ -43,20 +45,4 @@ module.exports = {
     <%= isWP ? "'chisel-plugin-wordpress'," : '' %>
     <%= isFE ? "'chisel-plugin-static-frontend'," : '' %>
   ],
-
-  // https://cli.vuejs.org/config/#configurewebpack
-  // configureWebpack(config) {},
-  // chainWebpack(config) {},
-
-  // Hooks can be used to change internal behavior, for example:
-  // documentation TBD :(
-  // hooks: {
-  //   wordPress: {
-  //     browserSyncConfig(config) {
-  //       // disable opening of browser window when dev server starts
-  //       // eslint-disable-next-line no-param-reassign
-  //       config.open = false;
-  //     },
-  //   },
-  // },
 }
