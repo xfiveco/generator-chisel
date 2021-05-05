4bd9abbbd31bcb762890647d093b3c653d9ab313
edenizk <dkokyanus@gmail.com>
2021-05-05 19:26:14 +0200

    script lint fixes

--- docs/src/scripts/app.js
+++ docs/src/scripts/app.js
@@ -2,11 +2,12 @@
   Project: Getchisel
   Author: Xfive
  */
-/* eslint-disable */
-// DISABLED ESLINT TEMP
 
 import Prism from 'prismjs';
 import Video from './modules/video';
 
 Prism.highlightAll();
-new Video();
+
+const components = [Video];
+
+Promise.resolve(components.forEach((Component) => new Component()));
