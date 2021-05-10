0cd562dbcf2080d8940a2c92b82b8e0dd9460f5d
edenizk <dkokyanus@gmail.com>
2021-05-10 19:06:02 +0200

    Add on page navigation script

--- docs/src/scripts/app.js
+++ docs/src/scripts/app.js
@@ -6,10 +6,11 @@
 import Prism from 'prismjs';
 import Video from './modules/video';
 import Sidebar from './modules/sidebar';
+import onPageNavigation from './modules/onPageNavigation';
 import {} from './helpers/inert';
 
 Prism.highlightAll();
 
-const components = [Video, Sidebar];
+const components = [Video, Sidebar, onPageNavigation];
 
 Promise.resolve(components.forEach((Component) => new Component()));
