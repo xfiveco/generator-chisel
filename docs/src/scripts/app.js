08193cadfe3599a3d604862c8d2c8fc01ab883a5
edenizk <dkokyanus@gmail.com>
2021-05-31 18:36:27 +0200

    Replace video component with embeded video

--- docs/src/scripts/app.js
+++ docs/src/scripts/app.js
@@ -6,12 +6,11 @@
 import hljs from 'highlight.js';
 import {} from './helpers/inert';
 
-import Video from './modules/video';
 import Sidebar from './modules/sidebar';
 import onPageNavigation from './modules/onPageNavigation';
 
 hljs.highlightAll();
 
-const components = [Video, Sidebar, onPageNavigation];
+const components = [Sidebar, onPageNavigation];
 
 Promise.resolve(components.forEach((Component) => new Component()));
