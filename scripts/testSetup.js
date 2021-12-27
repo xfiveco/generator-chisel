7d78a9f6b3a8df34f58a7ea3fb0ca8ed9326c040
Jakub Bogucki <jakub.bogucki@xfive.co>
2021-12-27 03:29:59 +0100

    remve logs

--- scripts/testSetup.js
+++ scripts/testSetup.js
@@ -25,10 +25,8 @@ global.chiselTestHelpers = {
       question += 1;
 
       const answers = answersGenerator(data);
-      const res = answers[question] ? answers[question]() : undefined;
-      console.log(questions, res);
 
-      return res;
+      return answers[question] ? answers[question]() : undefined;
     });
 
     return () => {
