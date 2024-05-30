d5cf033dec992b3c5ffc77ddd53cfa6fec8fa226
Jakub Bogucki <jakub.bogucki@xfive.co>
2024-05-30 21:29:51 +0200

    separate docs dependencies, cleanup

--- packages/generator-chisel/lib/commands/create/packages-versions.js
+++ packages/generator-chisel/lib/commands/create/packages-versions.js
@@ -1,7 +1,4 @@
 module.exports = {
-  'chisel-plugin-code-style': '1.0.0',
-  'chisel-plugin-static-frontend': '1.0.0',
-  'chisel-plugin-wordpress': '1.0.0',
   'chisel-scripts': '1.0.0',
   'chisel-shared-utils': '1.0.0',
   'generator-chisel': '1.0.0',
