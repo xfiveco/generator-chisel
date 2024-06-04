99979159bbe0275572382135e07bc86d64d92c76
Jakub Bogucki <jakub.bogucki@xfive.co>
2024-06-04 23:08:32 +0200

    fix dependencies

--- packages/generator-chisel/lib/commands/create/creators/app/index.js
+++ packages/generator-chisel/lib/commands/create/creators/app/index.js
@@ -81,15 +81,10 @@ module.exports = async (api) => {
     //   cwd: api.resolve(app.themePath),
     // });
 
-    const oldVal = process.env.npm_config_force;
-    process.env.npm_config_force = 'true';
-
     await run(
       ['npx', '--yes', '@xfive/coding-standards@latest', '--skip-staged-check'],
       { cwd: api.resolve(app.themePath) },
     );
-
-    process.env.npm_config_force = oldVal;
 
     if (api.creator.cmd.link) {
       const availablePackages = Object.keys(packagesVersions);
