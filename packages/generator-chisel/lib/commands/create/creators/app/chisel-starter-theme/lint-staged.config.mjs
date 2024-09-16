e51b4b6e26cb7658404c6892ebf89eb8aacf32d9
Jakub Bogucki <jakub.bogucki@xfive.co>
2024-09-16 13:04:20 +0200

    remove lint staged (it is in coding standards)

--- packages/generator-chisel/lib/commands/create/creators/app/chisel-starter-theme/lint-staged.config.mjs
+++
@@ -1,8 +0,0 @@
-const listStagedConfig = {
-  '*.{cjs,mjs,js,jsx,ts,tsx}': ['eslint --fix'],
-  '*.vue': ['stylelint --fix', 'eslint --fix --allow-empty-input'],
-  '*.{json,md,yml}': ['prettier --write'],
-  '*.{css,scss}': ['stylelint --fix --allow-empty-input'],
-};
-
-export default listStagedConfig;
