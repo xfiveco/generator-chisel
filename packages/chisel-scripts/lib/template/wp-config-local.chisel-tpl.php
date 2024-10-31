642f983b7505384da808837e84572a1c41a54aa1
Rafal Puczel <rafal.puczel@xfive.co>
2024-10-31 10:25:21 +0100

    composer, wp-config, plugins list updates

--- packages/chisel-scripts/lib/template/wp-config-local.chisel-tpl.php
+++ packages/chisel-scripts/lib/template/wp-config-local.chisel-tpl.php
@@ -47,9 +47,9 @@ $table_prefix = '<%= tablePrefix %>';
 define( 'WP_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
 define( 'WP_DEBUG_DISPLAY', true );
-define( 'SCRIPT_DEBUG', true );
 
 // Required for the theme fast refresh mode.
+define( 'SCRIPT_DEBUG', true );
 define( 'WP_ENVIRONMENT_TYPE', 'development' );
 
 /** The Database Collate type. Don't change this if in doubt. */
